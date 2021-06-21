f5f05a20855c2fbd8cd0ab261c0027cd2f1300a4
dan7sc <su7cheng@gmail.com>
2021-06-21 15:03:38 -0300

    Add phone row

--- lib/main.dart
+++ lib/main.dart
@@ -40,56 +40,75 @@ class _MyHomePageState extends State<MyHomePage> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Card(
-              child: Container(
-                height: 150.0,
-                width: 400.0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 16.0,
-                                  vertical: 8.0,
-                                ),
-                                child: Container(
-                                  width: 45.0,
-                                  height: 45.0,
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    shape: BoxShape.circle,
-                                  ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                                vertical: 8.0,
+                              ),
+                              child: Container(
+                                width: 45.0,
+                                height: 45.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  shape: BoxShape.circle,
                                 ),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 16.0),
-                                child: Container(
-                                  // width: 200,
-                                  child: Text(
-                                    "Daniel",
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 16.0),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                child: Text("Daniel",
                                     overflow: TextOverflow.ellipsis,
                                     style: TextStyle(
                                       fontSize: 24.0,
-                                    )
-                                  ),
-                                ),
+                                    )),
                               ),
-                            ],
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.phone,
+                            color: Colors.blue,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Icon(Icons.phone),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 16.0,
+                        ),
+                        Text(
+                          "Telefone : ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        Text("+55 (55) 55555-5555",
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            )),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
